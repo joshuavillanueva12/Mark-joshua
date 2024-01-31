10868a4c7ee4ce1424d55bca431086e29aaf79dd
joshuavillanueva12 <158235205+joshuavillanueva12@users.noreply.github.com>
2024-01-31 08:58:27 +0800

    program 8.cpp

---
+++ program 8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+char l ='A';
+int num = 123;
+float fare = 8.46f;
+cout<<"The value of letter is "<<l<<"\n";
+cout<<"The value if the number is "<<num<<"\n";
+cout<<"The value of the fare is "<<fare;
+
+return 0;
+}
